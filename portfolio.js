f1a258eba5999ae4351732622b3f2b0f5a1d6116
John Zolton <102374100+JohnZolton@users.noreply.github.com>
2023-01-02 15:10:59 -0500

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    //loadcontent()
     let vars = document.querySelectorAll('.tile')
     vars.forEach(tile => tile.addEventListener('click', () => {loadcontent(tile.id)}))
-    //vars.forEach('click', () => console.log('clicked'))
 
     let return_btns = document.querySelectorAll('.return')
     return_btns.forEach(btn => btn.addEventListener('click', () => show_index()))
